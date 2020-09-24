55bd0801f49324202ea48613553f2d2e725d3d2f
cjfaulkner <53609212+cjfaulkner@users.noreply.github.com>
2020-09-24 17:31:20 +0200

    Update IndexFragmentation.sql
    Added code to IndexFragmentation to automatically rebuild the indexes which are fragmented above a certain threshold

--- IndexFragmentation.sql
+++ IndexFragmentation.sql
@@ -1,33 +1,70 @@
-SELECT dbschemas.[name] as 'Schema',
-dbtables.[name] as 'Table',
-dbindexes.[name] as 'Index',
-indexstats.avg_fragmentation_in_percent,
-indexstats.page_count
-FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, NULL) AS indexstats
-INNER JOIN sys.tables dbtables on dbtables.[object_id] = indexstats.[object_id]
-INNER JOIN sys.schemas dbschemas on dbtables.[schema_id] = dbschemas.[schema_id]
-INNER JOIN sys.indexes AS dbindexes ON dbindexes.[object_id] = indexstats.[object_id]
-AND indexstats.index_id = dbindexes.index_id
-WHERE indexstats.database_id = DB_ID()
-AND dbschemas.[name] LIKE 'WIISE%'
-ORDER BY indexstats.avg_fragmentation_in_percent desc
+DROP TABLE IF EXISTS #IndexBuild
 
-/*
-ALTER INDEX 
-ON REF_POPULATIONS REBUILD
+DECLARE @SQL VARCHAR(MAX)
+DECLARE @TableName VARCHAR(255)
+DECLARE @SchemaName VARCHAR(255)
+DECLARE @IndexName VARCHAR(255)
+DECLARE @FragThreshold float = 80 -- To stop rebuilds set this to 110
+DECLARE @SchemaFilter VARCHAR(255) = 'wiise_store'
 
+SELECT
+	dbschemas.[name] as 'SchemaName',
+	dbtables.[name] as 'TableName',
+	dbindexes.[name] as 'IndexName',
+	indexstats.avg_fragmentation_in_percent,
+	indexstats.page_count
+INTO #IndexBuild
+FROM
+	sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, NULL) AS indexstats
+INNER JOIN
+	sys.tables dbtables
+ON
+	dbtables.[object_id] = indexstats.[object_id]
+INNER JOIN
+	sys.schemas dbschemas
+ON
+	dbtables.[schema_id] = dbschemas.[schema_id]
+INNER JOIN
+	sys.indexes AS dbindexes
+ON
+	dbindexes.[object_id] = indexstats.[object_id]
+AND
+	indexstats.index_id = dbindexes.index_id
+WHERE
+	indexstats.database_id = DB_ID()
+AND
+	dbschemas.[name] LIKE '%' + ISNULL(@SchemaFilter, '') + '%'
+AND
+	indexstats.avg_fragmentation_in_percent >= @FragThreshold
+ORDER BY
+	indexstats.avg_fragmentation_in_percent DESC
 
+SELECT * FROM #IndexBuild
 
+WHILE (SELECT COUNT(*) FROM #IndexBuild) > 0
+BEGIN
+	SELECT TOP 1
+		@TableName = TableName,
+		@SchemaName = SchemaName,
+		@IndexName = IndexName	
+	FROM
+		#IndexBuild
+	
+	SET @SQL = 'ALTER INDEX ' + @IndexName + ' ON ' + @SchemaName + '.' + @TableName + ' REBUILD'
 
-ALTER INDEX PK_NCOV_LINELIST
-ON ncov_store.NCOV_LINELIST REBUILD
+	RAISERROR(@SQL, 0, 0) WITH NOWAIT
 
-ALTER INDEX IX__Sys_BatchID
-ON ncov_store.NCOV_LINELIST REBUILD
+	EXEC (@SQL)
 
-ALTER INDEX IX__Sys_RowTitle
-ON ncov_store.NCOV_LINELIST REBUILD
-
-*/
+	DELETE
+	FROM
+		#IndexBuild
+	WHERE
+		TableName = @TableName
+	AND
+		SchemaName = @SchemaName
+	AND
+		IndexName = @IndexName	
+END
 
 --UPDATE STATISTICS ncov_store.NCOV_LINELIST
